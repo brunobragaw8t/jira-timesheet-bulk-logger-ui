da27a6028c9fb030f91ebc806b265cb18eeca7db
Bruno Braga <brunobragaw8t@gmail.com>
2023-10-30 21:04:39 +0000

    feature: add border to page slot

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="p-8">
     <AppTitle />
+
     <AppNav class="mt-4" />
-    <slot />
+
+    <div class="mt-4 rounded-xl border border-gray-800 p-6">
+      <slot />
+    </div>
   </div>
 </template>
