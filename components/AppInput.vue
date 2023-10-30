936200eaf1ed4380e46cea7ae862628cdf92a6af
Bruno Braga <brunobragaw8t@gmail.com>
2023-10-30 21:32:20 +0000

    feature: create AppInput and add 3 of them to settings page

---
+++ components/AppInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  type: string;
+  placeholder: string;
+  instructions?: string;
+}>()
+
+const value = defineModel()
+</script>
+
+<template>
+  <div>
+    <label :for="$.uid.toString()" class="block mb-1 tracking-tight text-sm font-medium">
+      {{ label }}
+    </label>
+
+    <input
+      :id="$.uid.toString()"
+      v-model="value"
+      :type="type"
+      :placeholder="placeholder"
+      class="block w-full rounded-md border border-gray-800 px-3 py-1 text-sm bg-transparent text-gray-200 focus-visible:outline-none"
+    >
+
+    <div v-if="instructions" class="block mt-1 text-gray-400 text-xs">
+      {{ instructions }}
+    </div>
+  </div>
+</template>
--- pages/settings.vue
+++ pages/settings.vue
@@ -5,5 +5,35 @@ useHead({
 </script>
 
 <template>
-  <h1>Settings</h1>
+  <div>
+    <div class="flex gap-4">
+      <AppInput
+        class="flex-1"
+        type="text"
+        label="Cloud ID"
+        placeholder="aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"
+        instructions="Your Atlassian cloud ID"
+      />
+
+      <AppInput
+        class="flex-1"
+        type="text"
+        label="Email"
+        placeholder="name.surname@company.com"
+        instructions="Email of your Atlassian account"
+      />
+
+      <AppInput
+        class="flex-1"
+        type="text"
+        label="API key"
+        placeholder="abcdefghijklmnopqrstuvwxyz1234567890"
+        instructions="API token generated in your account security section"
+      />
+    </div>
+
+    <div>
+      <button>Save</button>
+    </div>
+  </div>
 </template>
