355c62919c9bacbe5dbf9d5f4ca6325038ce947f
Bruno Braga <brunobragaw8t@gmail.com>
2023-11-01 11:03:58 +0000

    feature: create logic to save settings to local storage

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,13 +1,62 @@
 <script setup lang="ts">
+/**
+ * Config
+ */
+
 useHead({
   title: 'Settings - Jira Timesheet Bulk Logger UI'
 })
+
+/**
+ * State
+ */
+
+const loading = ref(true)
+
+const settings = ref({
+  cloudId: '',
+  email: '',
+  apiKey: ''
+})
+
+const displaySaved = ref(false)
+
+/**
+ * Actions
+ */
+
+function getSettings () {
+  const settingsFromStorage = localStorage.getItem('settings')
+
+  if (settingsFromStorage) {
+    settings.value = JSON.parse(settingsFromStorage)
+  }
+
+  loading.value = false
+}
+
+function saveSettings () {
+  localStorage.setItem('settings', JSON.stringify(settings.value))
+
+  displaySaved.value = true
+
+  setTimeout(() => {
+    displaySaved.value = false
+  }, 2000)
+}
+
+/**
+ * Lifecycle
+ */
+
+onMounted(getSettings)
 </script>
 
 <template>
-  <div>
+  <form @submit.prevent="saveSettings">
     <div class="flex gap-4">
       <AppInput
+        v-model="settings.cloudId"
         class="flex-1"
         type="text"
         label="Cloud ID"
@@ -16,6 +65,7 @@ useHead({
       />
 
       <AppInput
+        v-model="settings.email"
         class="flex-1"
         type="text"
         label="Email"
@@ -24,6 +74,7 @@ useHead({
       />
 
       <AppInput
+        v-model="settings.apiKey"
         class="flex-1"
         type="text"
         label="API key"
@@ -32,8 +83,13 @@ useHead({
       />
     </div>
 
-    <div>
-      <button>Save</button>
+    <div class="flex items-center gap-2 mt-6">
+      <AppButton text="Save" />
+
+      <div v-if="displaySaved" class="flex items-center gap-0.5">
+        <Icon name="material-symbols:check-small-rounded" color="green" />
+        <span class="tracking-tight text-sm font-medium">Saved!</span>
+      </div>
     </div>
-  </div>
+  </form>
 </template>
