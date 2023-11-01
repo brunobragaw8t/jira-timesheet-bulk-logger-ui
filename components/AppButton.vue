447b353d83d281a7275fe9ac67ceb2707920bd85
Bruno Braga <brunobragaw8t@gmail.com>
2023-11-01 11:03:47 +0000

    feature: create AppButton

---
+++ components/AppButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  text: string;
+}>()
+</script>
+
+<template>
+  <button class="rounded-md px-4 py-2 bg-gray-200 text-gray-800 text-sm font-medium transition-colors hover:bg-gray-400">
+    {{ text }}
+  </button>
+</template>
