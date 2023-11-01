8c6456fcec6999d15b0690a73ea06e1566848f4c
Bruno Braga <brunobragaw8t@gmail.com>
2023-11-01 14:28:50 +0000

    feature: present alert in index when settings aren't configured

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+/**
+ * Config
+ */
+
 useHead({
   title: 'Jira Timesheet Bulk Logger UI'
+})
+
+const settings = useSettings()
+
+/**
+ * State
+ */
+
+const settingsApplied = computed(() => {
+  return settings.data.value.cloudId && settings.data.value.email && settings.data.value.apiKey
 })
 </script>
 
 <template>
-  <h1>Index</h1>
+  <div>
+    <div v-if="!settingsApplied">
+      <div class="p-4 mb-4 text-sm rounded-lg bg-gray-800 text-blue-400" role="alert">
+        <span class="font-semibold">Heads up!</span>
+        You need to configure your settings before you can use this app.
+      </div>
+
+      <AppButton
+        text="Click here to go to settings"
+        @click="navigateTo('/settings')"
+      />
+    </div>
+
+    <h1 v-else>
+      Index
+    </h1>
+  </div>
 </template>
